0a189fdfb26d889c1598c161e861da639e8fa781
Kevin Morales <kfmorales94@gmail.com>
2025-04-01 13:45:40 -0500

    Project init

---
+++ DevLokosOnlineDocSwiftUI/DevLokosOnlineDocSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevLokosOnlineDocSwiftUIApp.swift
+//  DevLokosOnlineDocSwiftUI
+//
+//  Created by Kevinho Morales on 1/4/25.
+//
+
+import SwiftUI
+
+@main
+struct DevLokosOnlineDocSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
